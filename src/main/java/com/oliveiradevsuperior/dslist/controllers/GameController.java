cd3307d75385b58593c723a4b1ec5ebb43f9df23
Pedro Oliveira <Pedroaolive@gmail.com>
2023-11-15 17:48:55 -0300

    feat: create GameDTO and findById method in service and controller

--- src/main/java/com/oliveiradevsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/oliveiradevsuperior/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.oliveiradevsuperior.dslist.controllers;
 
+import com.oliveiradevsuperior.dslist.dto.GameDto;
 import com.oliveiradevsuperior.dslist.dto.GameMinDto;
 import com.oliveiradevsuperior.dslist.entities.Game;
 import com.oliveiradevsuperior.dslist.services.GameServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +19,16 @@ public class GameController {
     @Autowired
     private GameServices gameServices;
 
-    @GetMapping()
+    @GetMapping
     public List<GameMinDto> findAll() {
         List<GameMinDto> result = gameServices.findAll();
+
+        return result;
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDto findById(@PathVariable Long id) {
+        GameDto result = gameServices.findById(id);
 
         return result;
     }
--- src/main/java/com/oliveiradevsuperior/dslist/services/GameServices.java
+++ src/main/java/com/oliveiradevsuperior/dslist/services/GameServices.java
@@ -1,10 +1,12 @@
 package com.oliveiradevsuperior.dslist.services;
 
+import com.oliveiradevsuperior.dslist.dto.GameDto;
 import com.oliveiradevsuperior.dslist.dto.GameMinDto;
 import com.oliveiradevsuperior.dslist.entities.Game;
 import com.oliveiradevsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,9 +16,18 @@ public class GameServices {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDto> findAll() {
         List<Game> result = gameRepository.findAll();
         List<GameMinDto> dto = result.stream().map(x -> new GameMinDto(x)).toList();
+
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public GameDto findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        GameDto dto = new GameDto(result);
 
         return dto;
     }
